eb3a5605bcdc93c28c6955350da7348c14618d4b
Yota Toyama <raviqqe@gmail.com>
2017-11-13 16:34:58 +0900

    Add installation script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eo pipefail
+
+curl -fLo \
+     ~/.config/nvim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+pip3 install --user --upgrade neovim
+gem install --user-install neovim
+
+cp ./init.vim ~/.config/nvim/init.vim
+
+nvim +PlugUpgrade +PlugClean! +PlugUpdate +qall
